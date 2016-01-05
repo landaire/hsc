16de9efeef64f0880b28c5ff9fa94688e9b19b6e
Lander Brandt <landerbrandt@gmail.com>
2016-01-05 13:33:30 -0800

    Add keywords to tree

--- source/hsc/parse/parse.d
+++ source/hsc/parse/parse.d
@@ -7,13 +7,15 @@ import hsc.parse.ast.node;
 class Tree {
   string name;
   string text;
+  string[] keywords;
   Token[] tokens;
   Node[] _nodes;
 
-  this(string name, string text) {
+  this(string name, string text, string[] keywords) {
     this.name = name;
     this.tokens = tokens;
     this.text = text;
+    this.keywords = keywords;
   }
 
   public void parse() {
