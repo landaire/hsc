87e955b11deb511f20fcfeb58659c33eb6c5313d
Lander Brandt <landerbrandt@gmail.com>
2016-01-05 13:33:47 -0800

    Parse XML files to get keywords

--- source/app.d
+++ source/app.d
@@ -6,20 +6,25 @@ import hsc.xml;
 
 void main(string[] args)
 {
-  auto xml = cast(string)std.file.read("H3_Scripting.xml");
-  stdout.writeln(HaloScript.parseXml(xml));
-
   string file;
   auto info = getopt(args,
                      "file", &file);
-
   if (!file) {
     defaultGetoptPrinter("Usage:", info.options);
 
     return;
   }
 
-  Lexer lex = new Lexer(file, cast(string)read(file));
+  // Parse the xml file that gives us info about opcodes and such
+  auto xml = cast(string)std.file.read("H3_Scripting.xml");
+  auto script = HaloScript.parseXml(xml);
+
+  // These are our keywords
+  string[] keywords;
+  keywords ~= script.values.keys;
+  keywords ~= script.scriptTypes.keys;
+
+  Lexer lex = new Lexer(file, cast(string)read(file), keywords);
   lex.run();
 
   stdout.writeln(lex.items);
