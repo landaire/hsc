46135ce3827529f08358ab71de6b549d4cd96dff
Lander Brandt <landerbrandt@gmail.com>
2016-02-20 12:43:24 -0800

    Spaces -> tabs

--- source/app.d
+++ source/app.d
@@ -6,27 +6,27 @@ import hsc.xml;
 
 void main(string[] args)
 {
-  string file;
-  auto info = getopt(args,
-                     "file", &file);
-  if (!file) {
-    defaultGetoptPrinter("Usage:", info.options);
+	string file;
+	auto info = getopt(args,
+										 "file", &file);
+	if (!file) {
+		defaultGetoptPrinter("Usage:", info.options);
 
-    return;
-  }
+		return;
+	}
 
-  // Parse the xml file that gives us info about opcodes and such
-  auto xml = cast(string)std.file.read("H3_Scripting.xml");
-  auto script = HaloScript.parseXml(xml);
+	// Parse the xml file that gives us info about opcodes and such
+	auto xml = cast(string)std.file.read("H3_Scripting.xml");
+	auto script = HaloScript.parseXml(xml);
 
-  // These are our keywords
-  string[] keywords = ["global"];
-  keywords ~= script.values.keys;
-  keywords ~= script.scriptTypes.keys;
+	// These are our keywords
+	string[] keywords = ["global"];
+	keywords ~= script.values.keys;
+	keywords ~= script.scriptTypes.keys;
 
-  auto parser = new Parser(file, cast(string)read(file), keywords);
-  parser.parse();
+	auto parser = new Parser(file, cast(string)read(file), keywords);
+	parser.parse();
 
-  // stdout.writeln(parser.tokens);
-  stdout.writeln(parser.toString());
+	// stdout.writeln(parser.tokens);
+	stdout.writeln(parser.toString());
 }
